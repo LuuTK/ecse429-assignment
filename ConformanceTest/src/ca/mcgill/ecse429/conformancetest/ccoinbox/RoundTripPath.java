bcf22541de91a03f05d8fb760b64ddbadc911ee7
luutk <tuan-kiet.luu@mail.mcgill.ca>
2015-10-03 15:04:18 -0400

    put node in a different class

--- ConformanceTest/src/ca/mcgill/ecse429/conformancetest/ccoinbox/RoundTripPath.java
+++ ConformanceTest/src/ca/mcgill/ecse429/conformancetest/ccoinbox/RoundTripPath.java
@@ -14,26 +14,7 @@ import tuantests.Node;
 
 public class RoundTripPath {
 	
-	private static class Node {
-		private State name;
-		private Boolean isParent;
-		private Node parent;
-		private List<Node> children;
-		private int value;
-		private State state;
-		private Transition prevTrans;
 
-		public Node(State state, Transition prevTrans, ArrayList<Node> children) {
-			this.state = state;
-			this.prevTrans = prevTrans;
-			children = new ArrayList<Node>();
-		}
-
-		public void treeInstert(Node parentNode, Node childrenNode) {
-			parentNode.children.add(childrenNode);
-			childrenNode.parent = parentNode;
-		}
-	}
 	
 	public static void main(String[] agrs) throws IOException{
 		PersistenceStateMachine.loadStateMachine("ccoinbox.xml");
